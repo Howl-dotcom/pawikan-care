335ccf0c700a18bcb8e5cdd4428abcac057dbc5d
Howl-dotcom <melrojas5103@gmail.com>
2026-05-05 15:50:02 +0800

    restore blade files with secure_asset fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>PawikanCare</title>
-  <link rel="stylesheet" href="{{ secure_secure_asset\('css/style.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
   <!-- Leaflet -->
