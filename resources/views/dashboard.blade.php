ae05eada5c6f081cc298ddebf77447ce234d16a5
Howl-dotcom <melrojas5103@gmail.com>
2026-05-05 14:52:49 +0800

    restore style.css with secure_asset

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>PawikanCare</title>
+  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
   <!-- Leaflet -->
