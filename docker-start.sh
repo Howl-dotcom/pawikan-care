228ae8293792b9c3c87e561c567d492f0b4ef94d
Howl-dotcom <melrojas5103@gmail.com>
2026-05-05 12:05:19 +0800

    use migrate fresh to reset tables

--- docker-start.sh
+++ docker-start.sh
@@ -36,7 +36,7 @@ php artisan cache:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --force
+php artisan migrate:fresh --force
 php artisan db:seed --class=UserSeeder --force
 echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=${PORT:-10000}
